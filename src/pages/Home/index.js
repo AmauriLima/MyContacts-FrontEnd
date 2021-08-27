b8cbbdcdde1333cd4d6540ce44b14a44ebd4c1ff
AmauriLima <amauri.plimaj@gmail.com>
2021-08-27 00:59:41 -0300

    refactory: useEffect with async function

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,17 +24,23 @@ export default function Home() {
   )), [searchTerm, contacts]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
-      .then(async (response) => {
+        const response = await fetch(
+          `http://localhost:3001/contacts?orderBy=${orderBy}`,
+        );
         await delay(500);
         const json = await response.json();
         setContacts(json);
-      })
-      .finally(() => {
+      } catch (error) {
+        console.log('error', error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+    loadContacts();
   }, [orderBy]);
 
   function handleToggleOrderBy() {
