112db992bf4b768d67a61281d13c80004832df45
AmauriLima <amauri.plimaj@gmail.com>
2022-11-13 23:40:04 -0300

    feat: adds removeListener function'

--- src/lib/EventManager.js
+++ src/lib/EventManager.js
@@ -12,15 +12,48 @@ export default class EventManager {
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    const listeners = this.listeners[event];
+
+    if (!listeners) {
       return;
     }
 
-    this.listeners[event].forEach((listener) => {
+    listeners.forEach((listener) => {
       listener(payload);
     });
+  }
+
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners[event];
+    if (!listeners) {
+      return;
+    }
+
+    const filteredListeners = listeners.filter(
+      (listener) => listener !== listenerToRemove,
+    );
+
+    this.listeners[event] = filteredListeners;
   }
 }
 
 const toastEventManager = new EventManager();
+
+function addToast1(payload) {
+  console.log('addtoast listener1', payload);
+}
+
+function addToast2(payload) {
+  console.log('addtoast listener2', payload);
+}
+
+toastEventManager.on('addtoast', addToast1);
+toastEventManager.on('addtoast', addToast2);
+
+toastEventManager.emit('addtoast', { type: 'danger', text: 'Texto' });
+
+toastEventManager.removeListener('addtoast', addToast1);
+
+toastEventManager.emit('addtoast', 'depois de remover');
+
 console.log({ toastEventManager });
