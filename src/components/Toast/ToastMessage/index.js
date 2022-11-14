44e61d6af29ded597e4b36d71262ef6807a6c226
AmauriLima <amauri.plimaj@gmail.com>
2022-11-13 22:25:06 -0300

    feat: adds toast message component

---
+++ src/components/Toast/ToastMessage/index.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+import { Container } from './styles';
+
+import xCircleIcon from '../../../assets/images/x-circle.svg';
+import checkCircleIcon from '../../../assets/images/check-circle.svg';
+
+export default function ToastMessage(props) {
+  const { text, type } = props;
+
+  console.log(type);
+
+  return (
+    <Container type={type}>
+      {type === 'danger' && <img src={xCircleIcon} alt="X" />}
+      {type === 'success' && <img src={checkCircleIcon} alt="Check" />}
+      <strong>
+        {text}
+      </strong>
+    </Container>
+  );
+}
+
+ToastMessage.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['default', 'success', 'danger']),
+};
+
+ToastMessage.defaultProps = {
+  type: 'default',
+};
