964c6a87a30386d6e32daeb6e46a07a8f7923bfb
AmauriLima <amauri.plimaj@gmail.com>
2022-11-20 00:06:42 -0300

    refactor: implements container/presentation pattern on edit contact page

---
+++ src/pages/EditContact/Container.js
@@ -0,0 +1,69 @@
+import { useState, useEffect, useRef } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import Presentation from './Presentation';
+
+export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  const safeAsyncAction = useSafeAsyncAction();
+
+  async function handleSubmit(contact) {
+    try {
+      const updatedContact = await ContactsService.updateContact({ id, body: contact });
+
+      setContactName(updatedContact.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso.',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato.',
+      });
+    }
+  }
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setIsLoading(false);
+          setContactName(contact.name);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado',
+          });
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history, safeAsyncAction]);
+
+  return (
+    <Presentation
+      contactFormRef={contactFormRef}
+      contactName={contactName}
+      isLoading={isLoading}
+      onSubmit={handleSubmit}
+    />
+  );
+}
