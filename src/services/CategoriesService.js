b0f7e048c1735bb27095301e45d4a5af38477efd
Amauri Lima <amauri.plimaj@gmail.com>
2023-06-20 14:05:01 -0300

    updates dot.env

--- src/services/CategoriesService.js
+++ src/services/CategoriesService.js
@@ -3,7 +3,7 @@ import HttpClient from './utils/HttpClient';
 
 class CategoriesService {
   constructor() {
-    this.httpClient = new HttpClient(process.env.api_url);
+    this.httpClient = new HttpClient(process.env.REACT_APP_API_URL);
   }
 
   async listCategories(signal) {
